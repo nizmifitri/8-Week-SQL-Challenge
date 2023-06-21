6a2c2bd7691c3a3f9fda733586bffcc4ab466560
Nizmi Fitri Rahayu <115331893+nizmifitri@users.noreply.github.com>
2023-06-21 11:43:20 +0700

    Update casestudy_2A.sql

--- Case Study #2 - Pizza Runner/casestudy_2A.sql
+++ Case Study #2 - Pizza Runner/casestudy_2A.sql
@@ -4,15 +4,15 @@ Tools : PostgreSQL
    Case Study Questions
    --------------------*/
 
---How many pizzas were ordered?
+-- 1. How many pizzas were ordered?
 SELECT COUNT(order_id) AS "Amount of Pizzas Ordered"
 FROM pizza_runner.customer_orders;
 
---How many unique customer orders were made?
+-- 2. How many unique customer orders were made?
 SELECT COUNT(DISTINCT order_id) AS "Amount of Unique Customer Orders"
 FROM pizza_runner.customer_orders;
 
---How many successful orders were delivered by each runner?
+-- 3. How many successful orders were delivered by each runner?
 SELECT 
 	runner_id,
     COUNT(runner_id) AS "Amount of Successful Orders"
@@ -21,7 +21,7 @@ WHERE cancellation = ''
 GROUP BY runner_id
 ORDER BY runner_id;
 
---How many of each type of pizza was delivered?
+-- 4. How many of each type of pizza was delivered?
 SELECT 
 	pizza_id,
     COUNT(pizza_id) AS "Amount Delivered"
@@ -31,7 +31,7 @@ ON ro.order_id = co.order_id
 WHERE cancellation = ''
 GROUP BY pizza_id;
 
---How many Vegetarian and Meatlovers were ordered by each customer?
+-- 5. How many Vegetarian and Meatlovers were ordered by each customer?
 SELECT 
 	customer_id,
     pizza_name,
@@ -42,7 +42,7 @@ ON co.pizza_id = pn.pizza_id
 GROUP BY customer_id, pizza_name
 ORDER BY customer_id;
 
---What was the maximum number of pizzas delivered in a single order?
+-- 6. What was the maximum number of pizzas delivered in a single order?
 WITH pizza_delivered
 AS
 (SELECT co.order_id, COUNT(co.order_id) AS count_order
@@ -55,7 +55,7 @@ GROUP BY co.order_id)
 SELECT MAX(count_order) AS "Max Number of Pizzas Delivered in Single Order"
 FROM pizza_delivered;
 
---For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+-- 7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
 SELECT co.customer_id,
 SUM(CASE
 	WHEN co.exclusions <> '' or co.extras <> '' THEN 1
@@ -71,7 +71,7 @@ ON co.order_id = ro.order_id
 WHERE ro.distance <> ''
 GROUP BY co.customer_id;
 
---How many pizzas were delivered that had both exclusions and extras?
+-- 8. How many pizzas were delivered that had both exclusions and extras?
 WITH extras
 AS
 (SELECT *,
@@ -88,7 +88,7 @@ ORDER BY co.order_id)
 SELECT SUM(count_extras) AS "Amount of pizzas delivered that had both exclusions and extras"
 FROM extras;
 
---What was the total volume of pizzas ordered for each hour of the day?
+-- 9. What was the total volume of pizzas ordered for each hour of the day?
 SELECT
 	EXTRACT(HOUR FROM order_time) AS "Hour Of The Day",
 	COUNT(EXTRACT(HOUR FROM order_time))
@@ -96,7 +96,7 @@ FROM pizza_runner.customer_orders
 GROUP BY EXTRACT(HOUR FROM order_time)
 ORDER BY EXTRACT(HOUR FROM order_time);
 
---What was the volume of orders for each day of the week?
+-- 10. What was the volume of orders for each day of the week?
 SELECT
 	EXTRACT(DOW FROM order_time) AS "Day Of The Week",
 	COUNT(EXTRACT(DOW FROM order_time))
